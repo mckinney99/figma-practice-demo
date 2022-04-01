62d23d86ac28d8869490c2b64c7f91945e4fc9b6
eric <guest1@guest1s-air.lan>
2022-04-01 13:22:18 -0500

    updated images to svg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,13 +7,13 @@ export default function Nav() {
       <div className="menu-logo">
         <div className="image"></div>
       </div>
-      <div className="nav-item" id="nav-home">
+      <div className="nav-item normal-text" id="nav-home">
         <a href='#home-section'>Home</a>
       </div>
-      <div className="nav-item" id="nav-about">
+      <div className="nav-item normal-text" id="nav-about">
         <a href='#about-section'>About Us</a>
       </div>
-      <div className="nav-item" id="nav-contact">
+      <div className="nav-item normal-text" id="nav-contact">
         <a href='#contact-section'>Contact</a>
       </div>
     </div>
